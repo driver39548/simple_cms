49418d472a75ae1affee778a0ace61944e532f26
driver39548 <43621113+driver39548@users.noreply.github.com>
2019-05-17 14:54:50 +0000

    First version

---
+++ app/models/subject.rb
@@ -0,0 +1,22 @@
+class Subject < ActiveRecord::Base
+    
+    # Could delete related pages automatically
+    # whenever a subject is deleted:
+    # has_many :pages, :dependent => :destroy
+    
+    has_many :pages
+    
+    acts_as_list
+    
+    validates_presence_of :name
+    validates_length_of :name, :maximum => 255
+    
+    scope :visible, lambda { where(:visible => true) }
+    scope :invisible, lambda { where(:visible => false) }
+    scope :sorted, lambda { order("subjects.position ASC") }
+    scope :newest_first, lambda { order("subjects.created_at DESC") }
+    scope :search, lambda { |query|
+        where(["name LIKE ?", "%#{query}%"])
+    }
+    
+end
